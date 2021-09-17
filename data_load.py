a335104600780d82cec02736fa4eb9314f0bb8e2
Zhenhui.Jin <Kingwield@qq.com>
2021-09-17 08:27:27 +0800

    数据加载

--- data_load.py
+++ data_load.py
@@ -1,5 +1,8 @@
 import os
 
+import cv2
+
+import numpy as np
 import pandas as pd
 from pandas import DataFrame
 
@@ -8,28 +11,27 @@ import config
 
 class DataLoad:
     def __init__(self, train_path: str, test_path: str):
-        self.__train_images__ = {}  # {'PatientID':[ImgPaths]}
-        self.__test_images__ = {}  # {'PatientID':[ImgPaths]}
+        train_images = []  # [{'patient ID': id, 'ImgNumber': number, 'ImgPath': path}]
+        test_images = []  # [{'patient ID': id, 'ImgNumber': number, 'ImgPath': path}]
+        self.__load_all_image_path__(train_path, train_images)
+        self.__load_all_image_path__(test_path, test_images)
 
-        self.__load_all_image_path__(train_path, self.__train_images__)
-        self.__load_all_image_path__(test_path, self.__test_images__)
+        self.train_images = pd.DataFrame(train_images)
+        self.test_images = pd.DataFrame(test_images)
 
-        self.train_data: DataFrame = self.__load_csv__(train_path)
-        self.test_data: DataFrame = self.__load_csv__(test_path)
+        self.train_data: DataFrame = self.load_csv(train_path)
+        self.test_data: DataFrame = self.load_csv(test_path)
 
-    def get_images_train(self, patient_id: str) -> list:
+    def merge_csv_image(self):
         """
-        根据PatientID获取图像列表
-        :param patient_id: PatientID
-        :return: list
+        将CSV数据和对应图像路径合并
+        :return:
         """
-        return self.__train_images__.get(patient_id)
-
-    def get_images_test(self, patient_id: str) -> list:
-        return self.__test_images__.get(patient_id)
+        self.train_data: DataFrame = pd.merge(self.train_data, pd.DataFrame(self.train_images), on='patient ID')
+        self.test_data: DataFrame = pd.merge(self.test_data, pd.DataFrame(self.test_images), on='patient ID')
 
     @staticmethod
-    def __load_csv__(path) -> DataFrame:
+    def load_csv(path) -> DataFrame:
         """
         加载CSV数据集
         :param path: 路径
@@ -41,7 +43,7 @@ class DataLoad:
                 data = pd.read_csv(os.path.join(path, file))
                 return data
 
-    def __load_all_image_path__(self, path: str, images: dict):
+    def __load_all_image_path__(self, path: str, images: list):
         """
         加载所有图像路径
         :param path:路径
@@ -54,16 +56,29 @@ class DataLoad:
             if os.path.isdir(file_path):
                 self.__load_all_image_path__(file_path, images)
             elif file.endswith('.jpg'):
-                file_id = file.split('_')[0]
-                paths = []
-                if images.get(file_id):
-                    paths = images[file_id]
-                paths.append(file_path)
-                images[file_id] = paths
+                split = file.replace('.jpg', '').split('_')
+                file_id = split[0]
+                file_number = split[1]
+                images.append({'patient ID': file_id, 'ImgNumber': file_number, 'ImgPath': file_path})
+
+    @staticmethod
+    def read_image(path: str) -> np.ndarray:
+        """
+        使用OpenCV加载图像数据
+        :param path:
+        :return:
+        """
+        img: np.ndarray = cv2.imread(path)
+        # 裁剪
+        img = img[:500, 500:, :]
+        return img
 
 
 dataLoad = DataLoad(config.TRAIN_DATA_FILE, config.TEST_DATA_FILE)
 
 if __name__ == '__main__':
-    print(dataLoad.get_images_train('0000-0501L'))
+    print(dataLoad.train_images.head(3))
     print(dataLoad.train_data.info())
+    print(dataLoad.train_data.head(3))
+
+    dataLoad.train_data.to_csv('train.csv')
