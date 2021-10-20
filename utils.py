77ac8da6b7c74553d434357fa22f4c50f3c627e6
Zhenhui.Jin <zhenhui_jin@mtelnet.com>
2021-10-20 14:14:47 +0800

    更新

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import os
 import cv2
 
+import pandas as pd
+
 import config
 from config import TRAIN_DATA_FILE, DATA_PATH, TEST_DATA_FILE, TRAIN_DATA_FILE_NEW, TEST_DATA_FILE_NEW, \
     CST_TRAIN_DATA_FILE, CST_TEST_DATA_FILE
@@ -18,8 +20,18 @@ def load_all_image_path(path: str, images: list):
         file_path = os.path.join(path, file)
         if os.path.isdir(file_path):
             load_all_image_path(file_path, images)
-        elif file.endswith('.jpg'):
-            images.append(file_path)
+        elif file.endswith('.jpg') or file.endswith('.png'):
+            split = file.replace('.jpg', '').replace('.png', '').split('_')
+            file_id = split[0]
+            if len(split) > 2:
+                # _1_000000
+                file_number = split[1] + split[2]
+            else:
+                file_number = split[1]
+            images.append({'patient ID': file_id,
+                           'LR': file_id[-1],
+                           'ImgNumber': file_number,
+                           'ImgPath': file_path.replace(config.root_path + '\\', '').replace('\\', '/')})
 
 
 def read_image(path: str):
@@ -39,20 +51,26 @@ def write_image(path: str, img):
     return img
 
 
-def crop_all():
+def crop_all(train_path, test_path, new_train_path, new_test_path):
     """
-    裁剪
+    裁剪图像
+    :param train_path: 裁剪前路径
+    :param test_path: 裁剪前路径
+    :param new_train_path: 裁剪后路径
+    :param new_test_path: 裁剪后路径
     :return:
     """
     images = []
-    load_all_image_path(TRAIN_DATA_FILE, images)
-    for path in images:
-        write_image(os.path.join(TRAIN_DATA_FILE_NEW, os.path.split(path)[-1]), read_image(path))
+    load_all_image_path(train_path, images)
+    for image in images:
+        path = image['ImgPath']
+        write_image(os.path.join(new_train_path, os.path.split(path)[-1]), read_image(path))
 
     images = []
-    load_all_image_path(TEST_DATA_FILE, images)
-    for path in images:
-        write_image(os.path.join(TEST_DATA_FILE_NEW, os.path.split(path)[-1]), read_image(path))
+    load_all_image_path(test_path, images)
+    for image in images:
+        path = image['ImgPath']
+        write_image(os.path.join(new_test_path, os.path.split(path)[-1]), read_image(path))
 
 
 def processing_all():
@@ -66,7 +84,8 @@ def processing_all():
 
     images = []
     load_all_image_path(TRAIN_DATA_FILE_NEW, images)
-    for path in images:
+    for image in images:
+        path = image['ImgPath']
         print(path)
         img = process.blur_canny_cst(path)
         save_path = os.path.join(CST_TRAIN_DATA_FILE, os.path.split(path)[-1])
@@ -75,55 +94,62 @@ def processing_all():
     print('*' * 50)
     images = []
     load_all_image_path(TEST_DATA_FILE_NEW, images)
-    for path in images:
+    for image in images:
+        path = image['ImgPath']
         print(path)
         img = process.blur_canny_cst(path)
         save_path = os.path.join(CST_TEST_DATA_FILE, os.path.split(path)[-1])
         cv2.imwrite(save_path, img)
 
 
-def __load_all_image_path__(self, path: str, images: list):
+def load_csv(path) -> pd.DataFrame:
     """
-    加载所有图像路径
-    :param path:路径
-    :param images:
-    :return:
+    加载CSV数据集
+    :param path: 路径
+    :return: DataFrame
     """
     files = os.listdir(path)
     for file in files:
-        file_path = os.path.join(path, file)
-        if os.path.isdir(file_path):
-            self.__load_all_image_path__(file_path, images)
-        elif file.endswith('.jpg'):
-            split = file.replace('.jpg', '').split('_')
-            file_id = split[0]
-            if len(split) > 2:
-                file_number = split[1] + split[2][-3:]
-            else:
-                file_number = split[1]
-            # _1_000000
-            images.append({'patient ID': file_id,
-                           'LR': file_id[-1],
-                           'ImgNumber': file_number,
-                           'ImgPath': file_path.replace(config.root_path + '\\', '').replace('\\', '/')})
+        if file.endswith('.csv'):
+            data = pd.read_csv(os.path.join(path, file), header=0)
+            return data
 
 
-def before_after_to_csv():
-    dataLoad = DataLoad(TRAIN_DATA_FILE_NEW, TEST_DATA_FILE_NEW)
+def before_after_to_csv(train_path, test_path, save_path):
+    """
+    数据集分成前后两部分
+    :param train_path:
+    :param test_path:
+    :param save_path:
+    :return:
+    """
+    train_images = []
+    load_all_image_path(train_path, train_images)
 
-    __load_all_image_path__(train_path, train_images)
-    # __load_all_image_path__(test_path, test_images)
+    test_images = []
+    load_all_image_path(test_path, test_images)
 
     train_images = pd.DataFrame(train_images, columns=['patient ID', 'LR', 'ImgNumber', 'ImgPath'])
     test_images = pd.DataFrame(test_images, columns=['patient ID', 'LR', 'ImgNumber', 'ImgPath'])
 
-    train_data: DataFrame = pd.merge(self.get_train_csv(), self.train_images, on='patient ID')
-    test_data: DataFrame = pd.merge(self.get_test_csv(), self.test_images, on='patient ID')
+    train_data: pd.DataFrame = pd.merge(load_csv(train_path), train_images, on='patient ID')
+    test_data: pd.DataFrame = pd.merge(load_csv(test_path), test_images, on='patient ID')
 
-    number = dataLoad.train_data['ImgNumber'].apply(lambda num: int(num[0]))
-    train_data1 = dataLoad.train_data[dataLoad.train_data.ImgNumber]
+    number = train_data.train_data['ImgNumber'].apply(lambda num: int(num[0]))
+    train_data_before = train_data[number == 1]
+    train_data_after = train_data[number == 2]
+
+    train_data_before.to_csv(os.path.join(save_path, 'train_data_before.csv'), index=False)
+    train_data_after.to_csv(os.path.join(save_path, 'train_data_after.csv'), index=False)
+
+    number = test_data.train_data['ImgNumber'].apply(lambda num: int(num[0]))
+    test_data_before = test_data[number == 1]
+    test_data_after = test_data[number == 2]
+
+    test_data_before.to_csv(os.path.join(save_path, 'test_data_before.csv'), index=False)
+    test_data_after.to_csv(os.path.join(save_path, 'test_data_after.csv'), index=False)
 
 
 if __name__ == '__main__':
-    crop_all()
-    before_after_to_csv()
+    crop_all(TRAIN_DATA_FILE, TEST_DATA_FILE, TRAIN_DATA_FILE_NEW, TEST_DATA_FILE_NEW)
+    before_after_to_csv(TRAIN_DATA_FILE_NEW, TEST_DATA_FILE_NEW, DATA_PATH)
